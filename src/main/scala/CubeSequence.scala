0d69ae61f046fd7ceb1f9d100df13ed23b3be236
ek <evija.kvante@inbox.lv>
2022-03-21 20:53:49 +0200

    Initial commit

---
+++ src/main/scala/CubeSequence.scala
@@ -0,0 +1,27 @@
+import scala.io.StdIn.readLine
+
+object CubeSequence extends App {
+
+  //TODO ask user for starting number
+  //TODO ask user for ending numbers
+  //Calculate cubes of these integers including start and end AND store results in a sequence
+  //Print the saved sequence on screen
+
+  //extra challenge save odd cubes and print them
+
+  val startNumber = readLine("What is starting number? ").toInt
+  val endNumber = readLine("What is end number?").toInt
+
+  val numbers = (startNumber to endNumber)
+
+  println(numbers)
+val cubeSequence = scala.collection.mutable.ArrayBuffer[Int]()
+  for (number <- numbers) {
+    val cubeNumbers = number * number * number
+    println(cubeNumbers)
+
+    val cubeArray = scala.collection.mutable.ArraySeq(cubeNumbers)
+    println(cubeArray)
+
+  }
+}
